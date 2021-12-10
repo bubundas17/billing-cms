7a37bca5c1694cf780fd246b218778ba992509dc
Amit Mondal <androidone548@gmail.com>
2021-12-10 20:15:15 +0530

    env Schema extracted from app.js to configs/env.config.js

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import envSchema from 'env-schema';
 
-envSchema({
-  dotenv: true,
-  schema: {
-    type: 'object',
-    properties: {
-      NODE_ENV: { type: 'string', enum: ['development', 'production'] },
-      PORT: { type: 'number' },
-      MONGO_URI: { type: 'string' },
-    },
-    required: ['NODE_ENV', 'PORT', 'MONGO_URI'],
-  },
-});
+import env from './configs/env.config.js';
 
 const app = express();
-const port = process.env.PORT;
+const port = env.PORT;
 
 app.listen(port, async () => {
   console.log(`App listening on port ${port}`);
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(env.MONGO_URI);
     console.log('Database connected');
   } catch (error) {
     console.error(error);
