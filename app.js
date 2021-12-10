f4199214086425c4526f6135de93149c15cfe1e0
Amit Mondal <androidone548@gmail.com>
2021-12-10 16:02:41 +0530

    database connection

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 import express from 'express';
+import envSchema from 'env-schema';
+import mongoose from 'mongoose';
+
+envSchema({
+  dotenv: true,
+  schema: {
+    type: 'object',
+    properties: {
+      PORT: {
+        type: 'number',
+        default: 3000,
+      },
+      MONGO_URI: {
+        type: 'string',
+        default: 'mongodb://localhost:27017/test',
+      },
+    },
+    required: ['PORT', 'MONGO_URI'],
+  },
+});
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, async () => {
+  console.log(`App listening on port ${port}`);
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Database connected');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
