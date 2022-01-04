2357c6a36eaf29c34972d3f895e93dffc27488b3
Amit Mondal <androidone548@gmail.com>
2022-01-04 13:11:19 +0530

    setup sass build process on development stage

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
 import '@babel/register';
-import { src, dest, watch } from 'gulp';
+import { src, dest } from 'gulp';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
@@ -10,10 +10,6 @@ function buildStyles() {
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss())
     .pipe(dest('./src/assets/css'));
-}
-
-export default function () {
-  watch('src/assets/scss/**/*.scss', buildStyles);
 }
 
 export { buildStyles };
