8a10bda433c05849898cc19250a88a20eb3a8577
Bubun Das <bubundas17@gmail.com>
2022-01-05 20:00:36 +0530

    home and clientarea routes added

---
+++ src/routes/clientarea/clientarea.route.js
@@ -0,0 +1,11 @@
+import BaseRoute from '@routes/base.route';
+
+class Clientarea extends BaseRoute {
+  constructor() {
+    super('/clientarea');
+    this.init();
+  }
+  init() {}
+}
+
+export default Clientarea;
--- src/routes/home/home.route.js
+++ src/routes/home/home.route.js
@@ -5,18 +5,9 @@ class HomeRoute extends BaseRoute {
     super('/');
     this.init();
   }
-
   init() {
-    /**
-     * @description Render home page
-     *
-     * @param {object} req
-     * @param {object} res
-     */
     this.router.get('/', (req, res) => {
-      res.render('index', {
-        pathName: 'home',
-      });
+      res.send('Hello World!');
     });
   }
 }
