1fc31134ae7304226c925343b267f5bb6b4b2a88
Amit Mondal <androidone548@gmail.com>
2022-01-31 12:26:17 +0530

    tailwindcss removed and bootstrap & sass added

---
+++ src/assets/js/main.js
@@ -0,0 +1,24 @@
+const sideNav = document.querySelector('.side-nav');
+const mainContent = document.querySelector('.main-content');
+const menuButton = document.querySelector('.header__menu-button');
+const backdrops = document.querySelector('.backdrops');
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 992) {
+    sideNav.style.display = 'block';
+    backdrops.style.display = 'none';
+  } else {
+    sideNav.style.display = 'none';
+    backdrops.style.display = 'none';
+  }
+});
+
+menuButton.addEventListener('click', () => {
+  sideNav.style.display = 'block';
+  backdrops.style.display = 'block';
+});
+
+backdrops.addEventListener('click', () => {
+  sideNav.style.display = 'none';
+  backdrops.style.display = 'none';
+});
