78a8879380bbd49d809fc9587d91b154ca403943
Amit Mondal <androidone548@gmail.com>
2022-02-15 18:34:23 +0530

    password and repeatPassword match custom decorator

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -1,6 +1,8 @@
 import { prop } from '@typegoose/typegoose';
 import { IsAlpha, IsEmail, IsString, MinLength } from 'class-validator';
 
+import { Match } from '@decorators/match.decorator';
+
 class CreateUserDto {
   @IsString({ message: 'Name is not correct' })
   @IsAlpha(undefined, { message: 'Name is not correct' })
@@ -21,6 +23,9 @@ class CreateUserDto {
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   @prop({ required: true })
   password: string;
+
+  @Match('password', { message: 'Passwords do not match' })
+  repeatPassword: string;
 }
 
 export default CreateUserDto;
