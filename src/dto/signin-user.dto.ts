5e4387dced204dec7c66f660adf3813230ba15bd
amit548 <androidone548@gmail.com>
2022-02-17 18:22:25 +0530

    Login way updated

--- src/dto/signin-user.dto.ts
+++ src/dto/signin-user.dto.ts
@@ -1,14 +1,12 @@
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
 class SignInUserDto {
-  @IsString({ message: 'Username is not correct' })
-  @MinLength(3, { message: 'Username must be at least 3 characters long' })
+  @IsString({ message: 'Username/Email is not correct' })
+  @MinLength(3, {
+    message: 'Username/Email must be at least 3 characters long',
+  })
   @IsOptional()
   username: string;
-
-  @IsEmail(undefined, { message: 'Email is not correct' })
-  @IsOptional()
-  email: string;
 
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   password: string;
