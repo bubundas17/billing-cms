c5568f11d01f055845a9cb4729242bfc19ace08c
amit548 <androidone548@gmail.com>
2022-03-06 19:19:03 +0530

    Register form client side validation added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,45 +1,23 @@
-(function () {
-  'use strict';
+const sideNav = document.querySelector('.side-nav');
+const menuButton = document.querySelector('.header__menu-button');
+const backdrops = document.querySelector('.backdrops');
 
-  const sideNav = document.querySelector('.side-nav');
-  // const mainContent = document.querySelector('.main-content');
-  const menuButton = document.querySelector('.header__menu-button');
-  const backdrops = document.querySelector('.backdrops');
-
-  window.addEventListener('resize', () => {
-    if (window.innerWidth >= 992) {
-      sideNav.style.display = 'block';
-      backdrops.style.display = 'none';
-    } else {
-      sideNav.style.display = 'none';
-      backdrops.style.display = 'none';
-    }
-  });
-
-  menuButton.addEventListener('click', () => {
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 992) {
     sideNav.style.display = 'block';
-    backdrops.style.display = 'block';
-  });
-
-  backdrops.addEventListener('click', () => {
+    backdrops.style.display = 'none';
+  } else {
     sideNav.style.display = 'none';
     backdrops.style.display = 'none';
-  });
+  }
+});
 
-  const forms = document.querySelectorAll('.needs-validation');
+menuButton.addEventListener('click', () => {
+  sideNav.style.display = 'block';
+  backdrops.style.display = 'block';
+});
 
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      'submit',
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add('was-validated');
-      },
-      false,
-    );
-  });
-})();
+backdrops.addEventListener('click', () => {
+  sideNav.style.display = 'none';
+  backdrops.style.display = 'none';
+});
