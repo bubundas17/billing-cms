031bed47943127028ed79c5be0b8917b985082c3
Amit Mondal <androidone548@gmail.com>
2022-03-17 19:34:18 +0530

    gulp js build

--- themes/art-of-light/gulpfile.js
+++ themes/art-of-light/gulpfile.js
@@ -1,18 +1,27 @@
 'use strict';
 
-const gulp = require('gulp');
+const { src, watch, dest } = require('gulp');
 const path = require('path');
 const sass = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel');
 
 function buildStyles() {
-  return gulp
-    .src(path.join(__dirname, './assets/scss/**/*.scss'))
+  return src(path.join(__dirname, './assets/**/*.scss'))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(dest('./public/css'));
+}
+
+function buildScripts() {
+  return src(path.join(__dirname, './assets/**/*.js'))
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(dest('./public'));
 }
 
 exports.buildStyles = buildStyles;
 
 exports.default = function () {
-  gulp.watch(path.join(__dirname, './assets/scss/**/*.scss'), buildStyles);
+  buildStyles();
+  buildScripts();
+  watch(path.join(__dirname, './assets/**/*.scss'), buildStyles);
+  watch(path.join(__dirname, './assets/**/*.js'), buildScripts);
 };
